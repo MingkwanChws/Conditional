a5276a1d5c309b50f354d03d54ff83fb00fa9648
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2019-12-13 18:39:42 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int score = 2;
+            if (score > 5) {
+                System.out.println("More than 5");
+            }else{
+                System.out.println("Less than 5");
+            }
+    }
+}
